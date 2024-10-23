6461bbdcf2df046b889be5e95fd074497c839995
unknown <je.cabalparamo@ugto.mx>
2024-10-22 18:29:27 -0600

    Primeros Cambios de FrontEnd 1.0

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,11 @@ export default {
 
   target: 'static',
 
+  server: {
+    port: process.env.PORT, // default 3000
+    host: '0.0.0.0' // default: localhost
+  },
+
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
   ],
